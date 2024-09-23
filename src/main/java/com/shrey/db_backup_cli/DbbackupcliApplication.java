f15182e431d4a4cec23477d1ec3cf26d5fc476be
Saikat Das <saikatxshrey@gmail.com>
2024-09-23 11:34:43 +0530

    added boilerplate code

---
+++ src/main/java/com/shrey/db_backup_cli/DbbackupcliApplication.java
@@ -0,0 +1,13 @@
+package com.shrey.db_backup_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbbackupcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbbackupcliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrey/db_backup_cli/DbbackupcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrey.db_backup_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbbackupcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
